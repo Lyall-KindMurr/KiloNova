24fcda9cf1df2bbe1f5ba5d3b53e8f4dba42978b
Lyall-KindMurr <Lyall-KindMurr@users.noreply.github.com>
2019-11-15 12:50:05 +0000

    First push of the game
    borken background, but camera and player movement are in place.

---
+++ Assets/Scripts/Paralax.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paralax : MonoBehaviour
+{
+    private float height, startpos;
+    public GameObject cam;
+    public float paralaxEffect;
+
+
+    void Start()
+    {
+        startpos = transform.position.y;
+        height = GetComponent<SpriteRenderer>().bounds.size.y;
+    }
+
+    private void FixedUpdate()
+    {
+        Debug.Log("this is a test");
+
+        float dist = (cam.transform.position.y * paralaxEffect);
+        float connector = (cam.transform.position.y * (1 - paralaxEffect));
+        transform.position = new Vector3(transform.position.x, startpos + dist, transform.position.z);
+
+
+        if (connector > startpos + height)
+        {
+            startpos += height;
+        }
+        else if (connector < startpos - height)
+            startpos -= height;
+    }
+}
---
+++ Assets/Scripts/PlayerControlledControlledMovement.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class PlayerControlledControlledMovement : MonoBehaviour
+{
+    public float speed = 5.0f;
+    Rigidbody2D rigidbody2D;
+    void Start()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+
+        rigidbody2D.velocity = new Vector2(x, y) * speed;
+        rigidbody2D.angularVelocity = 0.0f;
+    }
+}
