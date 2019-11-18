bd1f14081e11d21210e5c950629ad9b92df1a098
Lyall-KindMurr <Lyall-KindMurr@users.noreply.github.com>
2019-11-18 13:42:43 +0000

    Getting bullets to work

---
+++ Assets/Scripts/BasicBullet.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class BasicBullet : MonoBehaviour
+{
+    public float moveSpeed = 100.0f;
+    public int damage = 5;
+    private void Start()
+    {
+        GetComponent<Rigidbody2D>().AddForce(transform.up * moveSpeed);
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        other.transform.SendMessage("TakeDamage", damage, SendMessageOptions.DontRequireReceiver);
+        Die();
+    }
+
+    private void OnBecameInvisible()
+    {
+        Die();
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/PlayerWeaponWeak.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class PlayerWeaponWeak : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn1;
+    public Transform bulletSpawn2;
+    public float fireTime = 0.25f;
+    bool fireLeft = false;
+
+    private bool isFiring = false;
+
+    private void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    private void Fire()
+    {
+        isFiring = true;
+            Instantiate(bulletPrefab, bulletSpawn1.position, bulletSpawn1.rotation);
+            Instantiate(bulletPrefab, bulletSpawn2.position, bulletSpawn2.rotation);
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            { Fire(); }
+        }
+    }
+}
