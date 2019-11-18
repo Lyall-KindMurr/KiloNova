0eaee2cec4d5c86dee297af7c680fc3394e400b2
Lyall-KindMurr <Lyall-KindMurr@users.noreply.github.com>
2019-11-18 15:09:10 +0000

    enemies work now!

---
+++ Assets/Scripts/PlayerWeaponHelix.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerWeaponHelix : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+    public Transform bulletSpawn1;
+    public float fireTime = 0.5f;
+    bool fireLeft = false;
+
+    private bool isFiring = false;
+
+    private void SetFiring()
+    {
+        isFiring = false;
+    }
+
+    private void Fire()
+    {
+        isFiring = true;
+        Instantiate(bulletPrefab, bulletSpawn1.position, bulletSpawn1.rotation);
+        Instantiate(bulletPrefab, bulletSpawn1.position, bulletSpawn1.rotation);
+        if (GetComponent<AudioSource>() != null)
+        {
+            GetComponent<AudioSource>().Play();
+        }
+        Invoke("SetFiring", fireTime);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (!isFiring)
+            { Fire(); }
+        }
+    }
+}
